ccc95e79d4e98113e36023387ece24a556a769b4
@BagliettoAron <aronbaglietto88@gmail.com>
2022-08-25 15:47:10 +0200

    add routes & dashbord in auth section

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.dashboard')
+
+@section('content')
+      <h2>Welcome</h2>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +20,12 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
+    
+Route::middleware('auth')
+        ->namespace('Admin')
+        ->name('admin.')
+        ->prefix('admin')
+        ->group(function() {
+            Route::get('/', 'HomeController@index')->name('home');
+        });
