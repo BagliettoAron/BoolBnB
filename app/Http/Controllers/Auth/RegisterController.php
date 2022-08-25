9dd6e380858fbc08a14a738cd041af931841f8f2
@BagliettoAron <aronbaglietto88@gmail.com>
2022-08-25 17:15:49 +0200

    add: surname & date of birth of registration form, fix: validation rules

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,7 +50,10 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['string', 'max:255', 'nullable'],
+            'last_name' => ['string', 'max:255', 'nullable'],
+            'date_of_birth' => ['date', 'nullable'],
+            // vogliamo mettere una validazione per evitare l'inserimento di date nel futuro con una funzione?
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
@@ -66,8 +69,11 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'last_name' => $data['last_name'],
+            'date_of_birth' => $data['date_of_birth'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+
         ]);
     }
 }
--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'date_of_birth', 'last_name'
     ];
 
     /**
