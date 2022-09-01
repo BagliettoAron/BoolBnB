de6ec3a4999de556acef16c8dec7eeb0a5e26a63
matte887 <m.erroi92@gmail.com>
2022-09-01 12:32:48 +0200

    Fix: bug on registration (password matching)

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,114 +1,133 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Register') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('register') }}">
+                            @csrf
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            <div class="form-group row">
+                                <label for="name"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}"  minlength="2" autocomplete="name" autofocus>
+                                <div class="col-md-6">
+                                    <input id="name" type="text"
+                                        class="form-control @error('name') is-invalid @enderror" name="name"
+                                        value="{{ old('name') }}" minlength="2" autocomplete="name" autofocus>
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                    @error('name')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="last_name" class="col-md-4 col-form-label text-md-right">{{ __('Lastname') }}</label>
+                            <div class="form-group row">
+                                <label for="last_name"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Lastname') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="last_name" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}"  minlength="2" autocomplete autofocus>
+                                <div class="col-md-6">
+                                    <input id="last_name" type="text"
+                                        class="form-control @error('last_name') is-invalid @enderror" name="last_name"
+                                        value="{{ old('last_name') }}" minlength="2" autocomplete autofocus>
 
-                                @error('last_name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                    @error('last_name')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="date_of_birth" class="col-md-4 col-form-label text-md-right">{{ __('Date of Birth') }}</label>
+                            <div class="form-group row">
+                                <label for="date_of_birth"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Date of Birth') }}</label>
 
-                            <div class="col-md-6">
-                                <input id="date_of_birth" type="date" class="form-control @error('date_of_birth') is-invalid @enderror" name="date_of_birth" value="{{ old('date_of_birth') }}" autofocus>
+                                <div class="col-md-6">
+                                    <input id="date_of_birth" type="date"
+                                        class="form-control @error('date_of_birth') is-invalid @enderror"
+                                        name="date_of_birth" value="{{ old('date_of_birth') }}" autofocus>
 
-                                @error('date_of_birth')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                    @error('date_of_birth')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }} *</label>
+                            <div class="form-group row">
+                                <label for="email"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }} *</label>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <div class="col-md-6">
+                                    <input id="email" type="email"
+                                        class="form-control @error('email') is-invalid @enderror" name="email"
+                                        value="{{ old('email') }}" required autocomplete="email">
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }} *</label>
+                            <div class="form-group row">
+                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}
+                                    *</label>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" onkeyup="checkPassword()" name="password" minlength="8" required autocomplete="new-password">
+                                <div class="col-md-6">
+                                    <input id="password" type="password"
+                                        class="form-control @error('password') is-invalid @enderror"
+                                        onkeyup="checkPassword()" name="password" minlength="8" required
+                                        autocomplete="new-password">
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }} *</label>
+                            <div class="form-group row">
+                                <label for="password-confirm"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }} *</label>
 
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" onkeyup="checkPassword()" minlength="8" required autocomplete="new-password">
-                                <span class="d-block" id="password-match"></span>
-                                <span id="password-length"></span>
+                                <div class="col-md-6">
+                                    <input id="password-confirm" type="password" class="form-control"
+                                        name="password_confirmation" onkeyup="checkPassword()" minlength="8" required
+                                        autocomplete="new-password">
+                                    <span class="d-block" id="password-match"></span>
+                                    <span id="password-length"></span>
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row py-3">
-                            <label class="col-md-4 col-form-label text-md-right">Required *</label>
-                        </div>
-
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary" id="submitButton" disabled>
-                                    {{ __('Register') }}
-                                </button>
+                            <div class="form-group row py-3">
+                                <label class="col-md-4 col-form-label text-md-right">Required *</label>
                             </div>
-                        </div>
-                    </form>
+
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary" id="submitButton" disabled>
+                                        {{ __('Register') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
 
 <script>
@@ -116,22 +135,27 @@
         const password = document.getElementById('password').value;
         const passwordConfirm = document.getElementById('password-confirm').value;
         const button = document.getElementById("submitButton");
-
-        if (password === passwordConfirm) {
-            document.getElementById('password-match').innerHTML = 'Password is matching';
-            button.removeAttribute('disabled');
-        } else if (password !== passwordConfirm) {
-            document.getElementById('password-match').innerHTML = 'Password is not matching';
-            button.setAttribute('disabled', '');
-        }
+        let minLength = false;
 
         if (password.length < 8) {
-            document.getElementById('password-length').innerHTML = 'Password must be at leat 8 characters';
-            button.setAttribute('disabled', '');
-        } else {
+            document.getElementById('password-length').innerHTML = 'Password must be at least 8 characters';
+            minLength = false;
+        } else if (password.length >= 8) {
             document.getElementById('password-length').innerHTML = '';
-            button.removeAttribute('disabled');
+            minLength = true;
+        }
+
+        if (password !== passwordConfirm) {
+            document.getElementById('password-match').innerHTML = 'Password is not matching';
+            button.setAttribute('disabled', '');
+        } else if (password === passwordConfirm) {
+            document.getElementById('password-match').innerHTML = '';
+            if (minLength) {
+                button.removeAttribute('disabled');
+            } else {
+                document.getElementById('password-length').innerHTML = 'Password must be at least 8 characters';
+                button.setAttribute('disabled', '');
+            }
         }
     }
-
 </script>
